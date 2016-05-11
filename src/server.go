79f3688006a653a193b2b1b8c2c3c2d5366f017e
Alberto <alberto.vaccari@smarp.com>
2016-05-11 13:14:30 +0300

    first commit

---
+++ src/server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/pat"
+)
+
+const (
+	PORT         = "1080"
+	VERSION_FILE = "version.dat"
+)
+
+var version string
+
+func calculateFibonacci(n int) uint64 {
+	a := uint64(0)
+	b := uint64(1)
+	// Iterate until desired position in sequence.
+	for i := 0; i < n; i++ {
+		// Use temporary variable to swap values.
+		temp := a
+		a = b
+		b = temp + a
+	}
+	return a
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	input := r.URL.Query().Get(":num")
+	num, err := strconv.Atoi(input)
+
+	if err == nil {
+		result := calculateFibonacci(num)
+		fmt.Fprintf(w, "{\"Version\" : \""+version+"\", \"Number\" : \""+strconv.Itoa(num)+"\", \"Result\": \""+strconv.FormatUint(result, 10)+"\"}")
+	} else {
+		fmt.Fprintf(w, "{\"Version\" : \""+version+"\", \"Error\": \"Entered number is invalid\"}")
+	}
+}
+
+func readVersionFile() {
+	v, err := ioutil.ReadFile(VERSION_FILE)
+
+	if err != nil {
+		panic(err) // Could not file file
+	}
+
+	version = strings.Replace(string(v), "\n", "", -1)
+}
+
+func main() {
+	// Use all available processing power
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+
+	readVersionFile()
+
+	// Serve and handle requests
+	r := pat.New()
+	r.Get("/fibo/{num}", DataHandler)
+	http.Handle("/", r)
+	fmt.Println("Serving on localhost:", PORT)
+	http.ListenAndServe(":"+PORT, nil)
+}
